bd6d350216806e4d40e5591ffb0b0103a3404c0d
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-12-27 17:58:28 +0100

    Finish cross toolchain

--- 02_LearningAboutToolchains/create-toolchain.sh
+++ 02_LearningAboutToolchains/create-toolchain.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -u
+
 
 thisScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -18,4 +21,30 @@ cd crosstool-ng
 ./configure --enable-local
 make
 make install
+
+# add the configuration
+cp -f ${thisScriptDir}/dot.config ${thisScriptDir}/crosstool-ng/.config
+# build the toolchain
+./ct-ng build
+
+
+# test
+export PATH=~/x-tools/arm-cortex_a8-linux-gnueabihf/bin/:$PATH
+cd ${thisScriptDir}
+
+# build example
+arm-cortex_a8-linux-gnueabihf-gcc helloworld.c -o helloworld
+
+# check if crosscompiled for ARM
+file helloworld | grep ARM
+
+# check configuration of cross-toolchain
+arm-cortex_a8-linux-gnueabihf-gcc -v &> gcc-config.txt
+grep -e "--with-sysroot=/home/$USER/x-tools/arm-cortex_a8-linux-gnueabihf/arm-cortex_a8-linux-gnueabihf/sysroot" gcc-config.txt
+grep -e "--enable-languages=c,c++" gcc-config.txt
+grep -e "--with-arch=armv7-a" gcc-config.txt
+grep -e "--with-cpu=cortex-a8" gcc-config.txt
+grep -e "--with-tune=cortex-a8" gcc-config.txt
+grep -e "--with-float=hard" gcc-config.txt
+grep -e "--enable-threads=posix" gcc-config.txt
 
