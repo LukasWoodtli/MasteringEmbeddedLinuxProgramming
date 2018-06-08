7b8f9c2d2e6e26659f5909f7ddbe3772a49da37d
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-06-08 11:40:42 +0200

    Build u-boot

---
+++ 03_AllAboutBootloaders/build_u_boot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -u
+
+
+thisScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+export PATH=~/x-tools/arm-cortex_a8-linux-gnueabihf/bin/:$PATH
+
+cd ${thisScriptDir}/u-boot
+
+# check if toolchain is available
+arm-cortex_a8-linux-gnueabihf-gcc --version #> /dev/null
+
+
+# build Das u-boot
+make CROSS_COMPILE=arm-cortex_a8-linux-gnueabihf- am335x_boneblack_defconfig
+make CROSS_COMPILE=arm-cortex_a8-linux-gnueabihf-
