cf783c172e034205a337f51fb61421b7175bb3d5
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-08-07 10:36:23 +0200

    Small improvement in docker command line

--- run-docker.sh
+++ run-docker.sh
@@ -6,4 +6,4 @@ set -u
 
 thisScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run -it --rm --privileged=true -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $UID:$(id -g) -v $(pwd):$thisScriptDir -w=$thisScriptDir lukaswoodtli/mastering_embedded_linux "$@"
+docker run -it --rm -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u):$(id -g) -v $(pwd):$thisScriptDir -w=$thisScriptDir lukaswoodtli/mastering_embedded_linux "$@"
