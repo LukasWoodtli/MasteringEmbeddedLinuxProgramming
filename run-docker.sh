c1bce51e65f1080e4cdb6fbed415b5844ba72815
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-09-12 11:10:29 +0200

    Use prebuilt docker container

--- run-docker.sh
+++ run-docker.sh
@@ -3,7 +3,12 @@
 set -e
 set -u
 
-
 thisScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker run -it --rm -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u):$(id -g) -v $(pwd):$thisScriptDir -w=$thisScriptDir lukaswoodtli/mastering_embedded_linux "$@"
+# get container from docker hub (docker.io)
+dockerContainer="docker.io/lukaswoodtli/masteringembeddedlinuxprogramming"
+
+# use locally built container (using build-docker.sh)
+#dockerContainer="lukaswoodtli/mastering_embedded_linux"
+
+docker run -it --rm -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u $(id -u):$(id -g) -v $(pwd):$thisScriptDir -w=$thisScriptDir $dockerContainer "$@"
