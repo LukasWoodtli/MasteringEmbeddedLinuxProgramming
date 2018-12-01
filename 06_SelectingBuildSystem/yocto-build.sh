7ea2c66e2e70b899039c5d6866d09227734eb3e8
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-12-01 20:54:10 +0100

    Build basic yocto qemu target

--- 06_SelectingBuildSystem/yocto-build.sh
+++ 06_SelectingBuildSystem/yocto-build.sh
@@ -13,7 +13,10 @@ set +u
 source oe-init-build-env build-quemuarm
 set -u
 
+cp -f "$thisScriptDir/qemuarm-local.conf" "$thisScriptDir/poky/build-quemuarm/conf/local.conf"
 
 # build
 bitbake core-image-minimal
 
+# run it with (might not work in docker container):
+# runqemu qemuarm nographic # end it with ctrl + a x
